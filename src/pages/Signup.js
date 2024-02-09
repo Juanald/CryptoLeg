9c497dc43d33f90f087ff873e1825527c86ef310
Gavin Dhaliwal <gavindhaliwal123@gmail.com>
2024-02-09 13:25:00 -0500

    Completed signup

--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,1 +1,78 @@
-export default function Signup() {}
+import { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+
+export default function Signup() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Here, you can perform authentication logic using the entered username and password
+    console.log("Username:", username);
+    console.log("Password:", password);
+    // You may also redirect the user to another page after successful login
+    navigate("/");
+  };
+
+  return (
+    <div className="flex flex-col h-screen items-center justify-center">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      >
+        <div className="mb-4">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="username"
+          >
+            Username
+          </label>
+          <input
+            className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="username"
+            type="text"
+            placeholder="Username"
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <div className="mb-6">
+          <label
+            className="block text-gray-700 text-sm font-bold mb-2"
+            htmlFor="password"
+          >
+            Password
+          </label>
+          <input
+            className="shadow appearance-none border border-red-500 rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="******************"
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <p className="text-red-500 text-xs italic">
+            Please choose a password.
+          </p>
+        </div>
+        <div className="flex items-center justify-between">
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="submit"
+          >
+            Sign Up
+          </button>
+          <Link
+            to="/login"
+            className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800 m-2"
+            href="#"
+          >
+            Have an account?
+          </Link>
+        </div>
+      </form>
+      <p className="text-center text-gray-500 text-xs">
+        @Gavyin thank you Tailwind
+      </p>
+    </div>
+  );
+}
