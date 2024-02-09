a19205b4f4cc5a4218cf5890325a19a25c76a829
Gavin Dhaliwal <gavindhaliwal123@gmail.com>
2024-02-09 13:34:12 -0500

    Added navbar to be worked on

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import Signup from "./pages/Signup";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
           <Route path="/" element={<Home></Home>} />
           <Route path="/login" element={<Login />} />
